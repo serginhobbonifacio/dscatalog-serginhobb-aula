36685e05a242a388be6f2ce3dc2bb4cdd32c537f
sergiobbonifacio <serginhobb2007@yahoo.com.br>
2022-02-10 14:22:53 -0300

    DTO

--- backend/src/main/java/com/serginhobb/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/serginhobb/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.serginhobb.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.serginhobb.dscatalog.dto.CategoryDTO;
 import com.serginhobb.dscatalog.entities.Category;
 import com.serginhobb.dscatalog.repositories.CategoryRepository;
 
@@ -16,8 +18,20 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)	
-	public List<Category> findAll(){
-		return repository.findAll();			
+	public List<CategoryDTO> findAll(){
+			
+		List<Category> list = repository.findAll();
+		
+		/*
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for (Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		
+		return listDto;
+		*/
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
