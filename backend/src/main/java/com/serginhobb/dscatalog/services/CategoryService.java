0ce53012684ae4ab6fedc40bea258fae4b6d2b22
sergiobbonifacio <serginhobb2007@yahoo.com.br>
2022-02-17 09:41:01 -0300

    Delete category

--- backend/src/main/java/com/serginhobb/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/serginhobb/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.serginhobb.dscatalog.dto.CategoryDTO;
 import com.serginhobb.dscatalog.entities.Category;
 import com.serginhobb.dscatalog.repositories.CategoryRepository;
+import com.serginhobb.dscatalog.services.exceptions.DatabaseException;
 import com.serginhobb.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -64,5 +67,18 @@ public class CategoryService {
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id); 
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 }
