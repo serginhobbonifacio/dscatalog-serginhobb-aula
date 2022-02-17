ffd17d9ddccfaa15f4a39efb7a365422074e09da
sergiobbonifacio <serginhobb2007@yahoo.com.br>
2022-02-17 15:44:26 -0300

    Pagination

--- backend/src/main/java/com/serginhobb/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/serginhobb/dscatalog/services/CategoryService.java
@@ -9,6 +9,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +27,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)	
-	public List<CategoryDTO> findAll(){
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
 			
-		List<Category> list = repository.findAll();
+		Page<Category> list = repository.findAll(pageRequest);
 		
 		/*
 		List<CategoryDTO> listDto = new ArrayList<>();
@@ -38,7 +40,7 @@ public class CategoryService {
 		return listDto;
 		*/
 		
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new CategoryDTO(x));
 	}
 
 	@Transactional(readOnly = true)	
